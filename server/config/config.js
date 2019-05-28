1be0a418262fc51caa77d29101af2338d6cd354a
Isaac Vázquez <isaacv2514@gmail.com>
2019-05-28 15:54:09 -0500

    Variable de entonrno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,12 +15,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ===================
 let urlDB;
 
-/* if ( process.env.NODE_ENV === 'dev' ) {
+if ( process.env.NODE_ENV === 'dev' ) {
 
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
- */
-    urlDB = 'mongodb+srv://ivazquez25:U25QeBFxOzLz7NXT@cluster0-4gtc6.mongodb.net/cafe';
-//}
+    
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
