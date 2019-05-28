15107400f93f5c5dcfc453d68e4529716431b047
Isaac Vázquez <isaacv2514@gmail.com>
2019-05-28 15:40:47 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //  Puerto
 // ===================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===================
+//  Entorno
+// ===================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===================
+//  Base de datos
+// ===================
+let urlDB;
+
+/* if ( process.env.NODE_ENV === 'dev' ) {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+ */
+    urlDB = 'mongodb+srv://ivazquez25:U25QeBFxOzLz7NXT@cluster0-4gtc6.mongodb.net/cafe';
+//}
+
+process.env.URLDB = urlDB;
