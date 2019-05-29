2c1693f1b30c3441bc8efb62a779180f6a3bb13b
Isaac Vázquez <isaacv2514@gmail.com>
2019-05-29 12:43:37 -0500

    hotfix

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use( express.static( path.resolve( __dirname, '../public' ) ) );
 
 // Configuración global de rutas
-app.use( require('./routes/index') );
+app.use( require('./routes/index.js') );
 
 mongoose.connect( process.env.URLDB,
     { useNewUrlParser: true, useCreateIndex: true },
